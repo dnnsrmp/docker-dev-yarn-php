cb9e9c2ab36ac57080f6a8aedbf807ee6b9024ce
DNNSRMP <dnnsrmp@gmail.com>
2019-01-11 15:08:17 +0100

    v7.3

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build . -t dnnsrmp/dev-yarn-php:latest
+docker build . -t dnnsrmp/dev-yarn-php:7.3
 docker push dnnsrmp/dev-yarn-php:7.3
-docker push dnnsrmp/dev-yarn-php:latest
+
